eee6b1ebdaa74ac07eef8073154a59348645bd43
Nic Hippenmeyer <nic@rinsed.co>
2020-12-28 23:25:14 -0600

    Enable selectize for select and audience fields (#3 and #5)

--- app/assets/javascripts/administrate/components/associative.js
+++ app/assets/javascripts/administrate/components/associative.js
@@ -1,5 +1,7 @@
 $(function() {
+  $('.field-unit--select select').selectize({});
   $('.field-unit--belongs-to select').selectize({});
   $(".field-unit--has-many select").selectize({});
   $('.field-unit--polymorphic select').selectize({});
+  $('.field-unit--audiences-field select').selectize({});
 });
