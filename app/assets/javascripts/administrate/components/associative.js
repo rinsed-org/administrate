d618ae98b608a81aa8a00c3f1556e4b1e9999ce6
Nic Hippenmeyer <nic@rinsed.co>
2021-12-08 18:00:13 -0500

    Enable selectize for voucher types field

--- app/assets/javascripts/administrate/components/associative.js
+++ app/assets/javascripts/administrate/components/associative.js
@@ -3,4 +3,5 @@ $(function() {
   $('.field-unit--belongs-to select').selectize({});
   $(".field-unit--has-many select").selectize({});
   $('.field-unit--audiences-field select').selectize({});
+  $('.field-unit--voucher-types-field select').selectize({});
 });
