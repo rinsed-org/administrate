c57a58b15f89a8c9db4930e599c89df4c804215e
Pablo Brasero <36066+pablobm@users.noreply.github.com>
2022-01-24 15:14:03 +0000

    Avoid Selenium warning about old options format (#2064)
    No idea of what I'm doing. Documentation is lacking. This seems
    to work. It's taken from https://chromedriver.chromium.org/capabilities

--- spec/support/webdrivers.rb
+++ spec/support/webdrivers.rb
@@ -5,10 +5,6 @@ Capybara.register_driver :chrome do |app|
 end
 
 Capybara.register_driver :headless_chrome do |app|
-  options = ::Selenium::WebDriver::Chrome::Options.new
-  options.headless!
-  options.add_argument "--window-size=1680,1050"
-
   Capybara::Selenium::Driver.new(
     app,
     browser: :chrome,
