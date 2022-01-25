5cf7719321eb541b92d3fa4f4691415c723a7949
Jordan Brough <jordan-brough@users.noreply.github.com>
2022-01-25 03:55:12 -0700

    Handle Selenium "options" deprecation (#2118)
    Silences this warning while running "bundle exec rspec":
    
        WARN Selenium [DEPRECATION] [:browser_options] :options as a parameter for
        driver initialization is deprecated. Use :capabilities with an Array of
        value capabilities/options if necessary instead.
    
    But with a cleaner approach from that introduced in #2064.

--- spec/support/webdrivers.rb
+++ spec/support/webdrivers.rb
@@ -5,6 +5,12 @@ Capybara.register_driver :chrome do |app|
 end
 
 Capybara.register_driver :headless_chrome do |app|
+  options = ::Selenium::WebDriver::Chrome::Options.new
+  options.headless!
+  options.add_argument "--window-size=1680,1050"
+  options.add_argument "--disable-gpu"
+  options.add_argument "--disable-dev-shm-usage"
+
   Capybara::Selenium::Driver.new(
     app,
     browser: :chrome,
