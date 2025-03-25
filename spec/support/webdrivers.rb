d5838e493f87b19647a35e56947929d7f9191975
Skylar Bolton <skylar.bolton@gmail.com>
2025-03-25 14:33:26 -0400

    Test forcing a chrome driver version

--- spec/support/webdrivers.rb
+++ spec/support/webdrivers.rb
@@ -1,4 +1,12 @@
 require "selenium/webdriver"
+require "webdrivers"
+
+# Set chromedriver version based on Ruby version to ensure CI compatibility
+if RUBY_VERSION.start_with?('3.0') || RUBY_VERSION.start_with?('3.1')
+  # For Ruby 3.0 and 3.1, use a specific compatible version
+  Webdrivers::Chromedriver.required_version = "123.0.6312.58"
+end
+# Ruby 2.7 seems to work with the default mechanism
 
 Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome)
@@ -10,6 +18,7 @@ Capybara.register_driver :headless_chrome do |app|
   options.add_argument "--window-size=1680,1050"
   options.add_argument "--disable-gpu"
   options.add_argument "--disable-dev-shm-usage"
+  options.add_argument "--no-sandbox"
 
   Capybara::Selenium::Driver.new(
     app,
