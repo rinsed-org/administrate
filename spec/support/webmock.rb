a9bf450578422dcd59afdd18a5b4d40868a03e35
Skylar Bolton <skylar.bolton@gmail.com>
2025-03-25 15:29:41 -0400

    Remove other selenium versions

--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -1,9 +1,5 @@
 require "webmock/rspec"
 
-# Allow downloading webdrivers for Selenium
-driver_hosts = Webdrivers::Common.subclasses.
-  map { |driver| URI(driver.base_url).host }
-
 # Downloading the Firefox driver involves a redirect
 driver_hosts = ["github-releases.githubusercontent.com"]
 
